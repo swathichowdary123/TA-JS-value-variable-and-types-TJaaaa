15473376e1c0c6e11c21390cb71ad2fb02d4d5f1
swathichowdary123 <swathichowdary548.com>
2023-11-05 14:52:21 +0530

    program based on loops

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -11,3 +11,33 @@
 // Calculate the sum of all numbers from 1 to 10.
 
 // Log all the values from 1 to 10 using while loop
+
+for(let i=1; i<10; i=i+1)
+{
+    console.log(i);
+}
+
+for(let i=1; i<10; i=i+1){
+    if(i%2===0){
+        console.log(i);
+    }
+}
+
+for(let i=1; i<10; i=i+1){
+    if(i%2!==0){
+        console.log(i);
+    }
+}
+let sum=0;
+for(let i=1; i<10; i++){
+    sum+=i;
+}
+console.log(sum);
+
+let sum1=0;
+let i=1;
+while(i<10){
+    sum1+=i;
+    i++;
+}
+console.log(sum1);
