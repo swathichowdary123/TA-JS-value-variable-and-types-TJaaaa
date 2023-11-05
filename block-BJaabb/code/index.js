f97c04d2d0a284d0718b02bcc3fa334cd0ad257a
swathichowdary123 <swathichowdary548.com>
2023-11-05 17:34:38 +0530

    programs based on logical operators loops

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,23 +5,52 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
-
+let user=Number(prompt("enter a number"));
+let userName=user;
+console.log(`the value of user is ${user}`);
+let age=22;
+let isLearning=true;
+user="john";
+console.log(`the value of user is ${user}`);
+console.log(`the value of userName is ${userName}`);
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
-
+let numA=Number(prompt("enter the first number"));
+let numB=Number(prompt("enter the second number"));
+let sum;
+sum=numA+numB;
+alert(sum);
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j=j+1)
+{
+    if(j%2==0){
+    console.log(j);
+    }
+}
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
-
+for(i=9; i<=9; i=i-1){
+if(i%2!==0){
+    console.log(i);
+}
+}
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
 
+let number=5;
+while(number>=0){
+    console.log(number);
+    number--;
+}
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
-
+let num=10;
+while(num>=0){
+    console.log(num);
+    num--;
+}
 // Comparisoin
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
@@ -36,3 +65,11 @@ Example:
 "hello", -21; // false
 
 */
+let num1=Number(prompt("enter first value"));
+let num2=Number(prompt("enter first value"));
+if(num1===num2){
+    alert("true");
+}
+else{
+    alert("enter a valid value");
+}
